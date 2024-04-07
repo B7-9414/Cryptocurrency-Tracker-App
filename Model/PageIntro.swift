393789821dc65b220c4b4efc73f7c642f4947cce
B7-9414 <banawi.bassam@gmail.com>
2024-04-07 15:19:28 -0500

    app logo and Walkthrough images

--- Model/PageIntro.swift
+++ Model/PageIntro.swift
@@ -17,8 +17,8 @@ struct PageIntro: Identifiable, Hashable {
 }
 
 var pageIntros: [PageIntro] = [
-    .init(introAssetImage: "Page 1", title: "Connect With\nCreators Easily", subTitle: "Thank you for choosing us, we can save your lovely time."),
-    .init(introAssetImage: "Page 2", title: "Get Inspiration\nFrom Creators", subTitle: "Find your favourite creator and get inspired by them."),
+    .init(introAssetImage: "Page 1", title: "Track your\nCryptocurrencies", subTitle: "Track cryptocurrencies and real value trade with ease and confidence in one safe and fast applications.Maximize your returns with our advanced optimization features. Our platform analyzes your portfolio and suggests adjustments to help you achieve your investment goals. "),
+    .init(introAssetImage: "Page 2", title: "Discover the Best\nCoins", subTitle: "Looking to explore the world of cryptocurrencies and find the best investment opportunities? Look no further! Our platform offers a comprehensive exploration of the top-performing coins and tokens in the market."),
     .init(introAssetImage: "Page 3", title: "Let's\nGet Started", subTitle: "To register for an account, kindly enter your details.", displaysAction: true),
 ]
 
